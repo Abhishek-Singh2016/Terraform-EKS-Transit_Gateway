54c90680154c68de020b17e64b32e6a039bc7cbf
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-15 12:53:16 +0530

    defined output in root module

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "password" {
+value = module.iam.password
+}
+
+
+output "access_key_id" {
+value = module.iam.access_key_id
+}
+
+output "secret_access_key" {
+value = module.iam.secret_access_key
+}
