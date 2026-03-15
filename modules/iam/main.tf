9eb0bece94077410bb8ed1802853a0c4a5b80acb
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-15 12:35:40 +0530

    retrieve keys and password for login

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -24,3 +24,24 @@ resource "aws_iam_user_policy" "admin" {
   user   = aws_iam_user.admin.name
   policy = data.aws_iam_policy_document.admin.json
 }
+
+#password for console login
+
+resource "aws_iam_user_login_profile" "admin" {
+  user    = aws_iam_user.admin.name
+  #pgp_key = "keybase:some_person_that_exists"
+}
+
+output "password" {
+value = aws_iam_user_login_profile.admin.encrypted_password
+}
+
+
+output "access_key_id" {
+value = aws_iam_access_key.example_user_key.id
+}
+
+output "secret_access_key" {
+value = aws_iam_access_key.example_user_key.secret
+sensitive = true
+}
