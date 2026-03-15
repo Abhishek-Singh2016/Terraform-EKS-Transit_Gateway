de2bbbb8286d486050612ec94fe95827b31331fe
Abhishek-Singh2016 <abhisheksingh050593@gmail.com>
2026-03-15 13:00:55 +0530

    added sensitive in output

--- output.tf
+++ output.tf
@@ -1,5 +1,6 @@
 output "password" {
 value = module.iam_user.password
+sensitive = true
 }
 
 
@@ -7,6 +8,9 @@ output "access_key_id" {
 value = module.iam_user.access_key_id
 }
 
+
+
 output "secret_access_key" {
 value = module.iam_user.secret_access_key
+sensitive = true
 }
